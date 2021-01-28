46bae8098324aca53df5f6d6b43f7900ca591de3
Mosae-Litsoane <mosaelitsoane@gmail.com>
2021-01-27 19:41:49 -0600

    Got most of the content on

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Main from './components/Main';
 import About from './components/About';
+import Contact from './components/Contact';
 function App() {
 	return (
 		<div className="App">
 			<Main />
 			<About />
+			<Contact />
 		</div>
 	);
 }
--- portfolio-site/src/components/About.jsx
+++ portfolio-site/src/components/About.jsx
@@ -1,5 +1,104 @@
 import React from 'react';
 import styled from 'styled-components';
+
+const StyledAboutSection = styled.section`
+	max-width: 900px;
+
+	.inner {
+		display: grid;
+		grid-template-columns: 3fr 2fr;
+		grid-gap: 50px;
+
+		@media (max-width: 768px) {
+			display: block;
+		}
+	}
+`;
+
+const StyledText = styled.div`
+ul.skills_list {
+    display: grid;
+    grid-template-columns: repeat(2,minmax(140px,200px));
+    padding: 0;
+    margin:20px 0 0 0;
+    overflow: hidden;
+    list-style:none;
+
+    li {
+        position:relative;
+        margin-bottom: 10px;
+        padding-left:20px;
+        font-size: xs
+
+        &:before {
+            content: '>';
+            position:absolute;
+            left:0;
+            color: blue;
+            line-height: 12px
+        }
+    }
+}
+`;
+// const StyledPic = styled.div`
+// 	position: relative;
+// 	max-width: 300px;
+// 	@media (max-width: 768px) {
+// 		margin: 50px auto 0;
+// 		width: 70%;
+// 	}
+// 	.wrapper {
+// 		${({ theme }) => theme.mixins.boxShadow};
+// 		display: block;
+// 		position: relative;
+// 		width: 100%;
+// 		border-radius: var(--border-radius);
+// 		background-color: var(--green);
+// 		&:hover,
+// 		&:focus {
+// 			background: transparent;
+// 			outline: 0;
+// 			&:after {
+// 				top: 15px;
+// 				left: 15px;
+// 			}
+// 			.img {
+// 				filter: none;
+// 				mix-blend-mode: normal;
+// 			}
+// 		}
+// 		.img {
+// 			position: relative;
+// 			border-radius: var(--border-radius);
+// 			mix-blend-mode: multiply;
+// 			filter: grayscale(100%) contrast(1);
+// 			transition: var(--transition);
+// 		}
+// 		&:before,
+// 		&:after {
+// 			content: '';
+// 			display: block;
+// 			position: absolute;
+// 			width: 100%;
+// 			height: 100%;
+// 			border-radius: var(--border-radius);
+// 			transition: var(--transition);
+// 		}
+// 		&:before {
+// 			top: 0;
+// 			left: 0;
+// 			background-color: var(--navy);
+// 			mix-blend-mode: screen;
+// 		}
+// 		&:after {
+// 			border: 2px solid var(--green);
+// 			top: 20px;
+// 			left: 20px;
+// 			z-index: -1;
+// 		}
+// 	}
+// `;
+
 function About() {
 	const skills = [
 		'JavaScript (ES6)',
@@ -9,32 +108,44 @@ function About() {
 		'WordPress',
 		'Angular',
 	];
+	const tools = [
+		'JavaScript (ES6)',
+		'React',
+		'Node.Js',
+		'HTML & (S)CSS',
+		'WordPress',
+		'Angular',
+	];
 	return (
-		<div id="about">
-			<h2 className="numbered_heading">About Me</h2>
+		<StyledAboutSection id="about">
+			<h2 className="numbered_heading">
+				<span>01.</span> About Me
+			</h2>
 
 			<div className="inner">
-				{/* styled text */}
-				<div>
-					<p>
-						Hi, I am Mosae Litsoane, I am a software developer. I build custom
-						web application and websites. I enjoy learning new technologies and
-						improving my skills
-					</p>
-					<p>
-						These are some of the languages, tools and frameworks I work with:
-					</p>
-				</div>
-				<ul className="skills-list">
-					{skills && skills.map((skills, i) => <li key={i}>{skills}</li>)}
-				</ul>
+				<StyledText>
+					<div>
+						<p>
+							Hi, I am Mosae Litsoane, I am a software developer. I build custom
+							web application and websites. I enjoy learning new technologies
+							and improving my skills
+						</p>
+						<p>
+							These are some of the languages, tools and frameworks I work with:
+						</p>
+					</div>
+					<ul className="skills-list">
+						<h3>Skills</h3>
+						{skills && skills.map((skills, i) => <li key={i}>{skills}</li>)}
+					</ul>
+				</StyledText>
 				{/* end of styled text */}
 				{/* start of styled pic */}
 				<div className="wrapper">
 					<img src="#" alt="Mosae" />
 				</div>
 			</div>
-		</div>
+		</StyledAboutSection>
 	);
 }
 
